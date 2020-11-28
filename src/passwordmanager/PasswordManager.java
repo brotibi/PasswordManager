ed593b100281df99f0e97c839db6c2b695b65fca
Anthony Gavazzi <gavazzi.a@husky.neu.edu>
2020-11-28 12:07:33 -0500

    added example view and src directory structure

---
+++ src/passwordmanager/PasswordManager.java
@@ -0,0 +1,9 @@
+package passwordmanager;
+
+/**
+ * Main program entry class for creating a Model, View, and Controller, and running
+ * the password manager to completion.
+ */
+public class PasswordManager {
+    public static void main(String[] args) {}
+}
